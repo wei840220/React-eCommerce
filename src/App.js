c7dbf241f67d87ef63df4625f055d4bcc451336e
Wei <wei840220@gmail.com>
2021-02-20 16:44:58 +0800

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from "./pages/homepage/homepage.component";
 
+const TopicsList = (props) => {
+  return (
+    <div>
+      <h1>{ `${props.match.params.topics.toUpperCase()} PAGE` }</h1>
+    </div>
+  );
+};
+
+const TopicsDatail = (props) => {
+  return (
+    <div>
+      <h1>{ `${props.match.params.topics.toUpperCase()} PAGE: ${props.match.params.topicId}` }</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}></Route>
+        <Route exact path='/:topics' component={TopicsList}></Route>
+        <Route path='/:topics/:topicId' component={TopicsDatail}></Route>
+      </Switch>
     </div>
   );
 }
